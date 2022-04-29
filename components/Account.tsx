28267c468be4691fefbdb16bf91019cec9c90e05
Dimitar Nestorov <8790386+dimitarnestorov@users.noreply.github.com>
2022-04-29 18:40:55 +0300

    Update account stats design

--- components/Account.tsx
+++ components/Account.tsx
@@ -2,38 +2,47 @@ import { Button } from './Button'
 import { useActiveConnector } from '../app'
 import { useDispatch } from 'react-redux'
 import { clearLastConnectorName } from '../features/web3/web3Slice'
+import { ReactNode } from 'react'
+import {
+    BsBrightnessHighFill,
+    BsFillClockFill,
+    BsFillCheckCircleFill,
+} from 'react-icons/bs'
+import { FaHeart } from 'react-icons/fa'
 
 export function Account() {
     const dispatch = useDispatch()
     const connector = useActiveConnector()!
 
     return (
-        <div>
+        <>
             <style jsx>{`
-                .box {
-                    border: 1px solid grey;
-                    border-radius: 6px;
-                    max-width: 200px;
-                    margin: 20px auto;
-                    text-align: center;
+                .stats {
+                    display: flex;
+                    justify-content: center;
+                    flex-wrap: wrap;
                 }
             `}</style>
-
-            <div className="box">
-                <h3>$0</h3>
-                <h4>Lent</h4>
-            </div>
-            <div className="box">
-                <h3>$0</h3>
-                <h4>Projected Earning PA</h4>
-            </div>
-            <div className="box">
-                <h3>0%</h3>
-                <h4>Projected APY</h4>
-            </div>
-            <div className="box">
-                <h3>0</h3>
-                <h4>Pools Supported</h4>
+            <div className="stats">
+                <Stat value="$0" name="Lent" icon={<BsFillCheckCircleFill />} />
+                <Stat
+                    value="$0"
+                    name="Projected Earning PA"
+                    icon={<BsFillClockFill />}
+                    color="blue"
+                />
+                <Stat
+                    value="0%"
+                    name="Projected APY"
+                    icon={<BsBrightnessHighFill />}
+                    color="yellow"
+                />
+                <Stat
+                    value="0"
+                    name="Pools Supported"
+                    icon={<FaHeart />}
+                    color="red"
+                />
             </div>
 
             <Button
@@ -51,6 +60,115 @@ export function Account() {
             >
                 Disconnect
             </Button>
-        </div>
+        </>
+    )
+}
+
+function Stat({
+    value,
+    name,
+    icon,
+    color = 'green',
+}: {
+    value: string
+    name: string
+    icon: ReactNode
+    color?: 'green' | 'blue' | 'yellow' | 'red'
+}) {
+    return (
+        <>
+            <style jsx>{`
+                .container {
+                    border-radius: 10px;
+                    width: 250px;
+                    height: 250px;
+                    margin: 30px;
+                    text-align: center;
+                    padding-top: 40px;
+
+                    > .icon {
+                        border-radius: 50%;
+                        margin: 0 auto 35px;
+                        width: 64px;
+                        height: 64px;
+                        padding: 20px;
+                        font-size: 24px;
+                    }
+
+                    > h3 {
+                        margin: 0;
+                        font-size: 32px;
+                        font-weight: 500;
+                    }
+
+                    > .name {
+                        margin-top: 12px;
+                        font-size: 14px;
+                    }
+
+                    &.green {
+                        background-color: #c8facd;
+                        color: #0b5248;
+
+                        > .icon {
+                            background-image: linear-gradient(
+                                315deg,
+                                #a0e1b5 0%,
+                                rgba(0, 0, 0, 0) 100%
+                            );
+                            color: #117b56;
+                        }
+                    }
+
+                    &.blue {
+                        background-color: #d0f2ff;
+                        color: #14297b;
+
+                        > .icon {
+                            background-image: linear-gradient(
+                                315deg,
+                                #aaddff 0%,
+                                rgba(0, 0, 0, 0) 100%
+                            );
+                            color: #3790ff;
+                        }
+                    }
+
+                    &.yellow {
+                        background-color: #fdf7cd;
+                        color: #7a4f01;
+
+                        > .icon {
+                            background-image: linear-gradient(
+                                315deg,
+                                #f1dfa5 0%,
+                                rgba(0, 0, 0, 0) 100%
+                            );
+                            color: #b78105;
+                        }
+                    }
+
+                    &.red {
+                        background-color: #fee7d9;
+                        color: #7a0c2e;
+
+                        > .icon {
+                            background-image: linear-gradient(
+                                315deg,
+                                #f0bdb6 0%,
+                                rgba(0, 0, 0, 0) 100%
+                            );
+                            color: #b72137;
+                        }
+                    }
+                }
+            `}</style>
+
+            <div className={`container ${color}`}>
+                <div className="icon">{icon}</div>
+                <h3>{value}</h3>
+                <div className="name">{name}</div>
+            </div>
+        </>
     )
 }
