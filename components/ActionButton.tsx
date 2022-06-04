3b950e300e2632e0a5e6412f19178066dd261b5d
Dimitar Nestorov <8790386+dimitarnestorov@users.noreply.github.com>
2022-06-04 13:12:56 +0300

    Fix loan action button stuck on loading when user cancels tx

--- components/ActionButton.tsx
+++ components/ActionButton.tsx
@@ -1,19 +1,14 @@
 import { ComponentProps, ReactNode, useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { AnyAction } from 'redux'
 import { Button } from './Button'
 
 export function ActionButton<T>({
     action,
-    onSuccess,
     children,
     ...props
 }: Omit<ComponentProps<Button>, 'onClick' | 'disabled'> & {
     action: () => Promise<T>
-    onSuccess?: (response: T) => AnyAction | Promise<AnyAction>
     children: ReactNode
 }) {
-    const dispatch = useDispatch()
     const [loading, setLoading] = useState(false)
     return (
         <Button
@@ -22,13 +17,10 @@ export function ActionButton<T>({
             loading={loading}
             onClick={() => {
                 setLoading(true)
-                if (onSuccess) {
-                    action().then(async (response) => {
-                        dispatch(await onSuccess(response))
-                        setLoading(false)
-                    })
-                } else {
-                    action()
+                action().then(doneLoading).catch(doneLoading)
+
+                function doneLoading() {
+                    setLoading(false)
                 }
             }}
         >
