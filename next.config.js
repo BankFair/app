c6a86c404b3d27b318bb19e732ff541027fa6c91
Farrukh Yakubov <fyakubov@users.noreply.github.com>
2022-09-07 13:29:06 +0600

    add training pool

--- next.config.js
+++ next.config.js
@@ -13,6 +13,13 @@ module.exports = {
                 description:
                     "The Kitale Community Pool lends to businesses in the Kitale area in Uganda. The most promising businesses are chosen based on the commercial outcome and the impact the growing business will have on job creation. The Pool Manager has been making loans for the last 10 years with zero default. Local businesses are chosen based on sustainability, the history of the business owner, and financially sound. The Pool Manager takes security and personal guarantees from the business owners. The security taken by the Pool Manager is normally the deed of their property. The Pool Manager aims to grow their stake in the pool and thus keep growing the pool. Some of the Pool Manager profits will be used as a contribution to the running costs of the Kitale Community School. In very few cases our borrowers can't access loans through local banks and if they can, the rates offered are significantly higher than the rates they can get through this lending pool.",
             },
+            {
+                name: 'Kitale Community Training Pool',
+                address: '0x063527eeB60ba6E6240b898315cee9E637CABe13',
+                block: 27974361,
+                description:
+                    "Training Pool",
+            },
         ]),
     },
     basePath: buildingForGitHubPages ? '/app' : '',
