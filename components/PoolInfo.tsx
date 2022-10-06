b854397372b50d85f97969ee90b32f2725724486
Farrukh Yakubov <fyakubov@users.noreply.github.com>
2022-10-06 15:23:48 +0100

    pool info on earn page

--- components/PoolInfo.tsx
+++ components/PoolInfo.tsx
@@ -5,9 +5,15 @@ import { PoolDescription } from './PoolDescription'
 export function PoolInfo({
     poolAddress,
     name,
+    managerName,
+    tokenSymbol,
+    uniswapUrl
 }: {
     poolAddress: string
     name: string
+    managerName: string
+    tokenSymbol: string
+    uniswapUrl: string
 }) {
     return (
         <Box>
@@ -24,19 +30,33 @@ export function PoolInfo({
                     margin: 24px 0 0;
                     display: flex;
                     align-items: center;
-
-                    > img {
+                    flex-wrap: wrap;
+                    
+                    > span {
+                      height: 24px;
+                      margin-right: 24px;
+                      margin-top: 12px;
+                      display: flex;
+                      align-items: center;
+                      
+                      > img {
                         width: 24px;
                         height: 24px;
                         margin-right: 8px;
+                      }
                     }
                 }
             `}</style>
             <h2>{name}</h2>
             <PoolDescription address={poolAddress} />
             <div className="subtitle">
-                <img src={`${prefix}/usdt.svg`} alt="USDT logo" />
-                USDT
+                <span>
+                    <img src={`${prefix}/usdt.svg`} alt="USDT logo" />
+                    USDT
+                </span>
+                {managerName ? <span>Pool Manager: {managerName}</span>: <></>}
+                {tokenSymbol ? <span>Token: {tokenSymbol}</span>: <></>}
+                {uniswapUrl ? <span><a href={uniswapUrl} target={"_blank"} rel="noreferrer">Uniswap pool</a></span>: <></>}
             </div>
         </Box>
     )
