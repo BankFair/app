111ad85fb2b18dced3ff684dac793d82a53b81fe
Farrukh Yakubov <fyakubov@users.noreply.github.com>
2022-10-16 03:23:33 +0100

    separate Staking page from loan management

--- pages/manage/[address].tsx
+++ pages/manage/[address].tsx
@@ -113,11 +113,6 @@ const Manage: NextPage<{ address: string }> = ({ address }) => {
             {pool ? (
                 pool.managerAddress === account ? (
                     <>
-                        <StakeAndUnstake
-                            pool={pool}
-                            poolAddress={address}
-                            account={account}
-                        />
                         <LoansAwaitingApproval
                             pool={pool}
                             poolAddress={address as Address}
@@ -140,74 +135,6 @@ Manage.getInitialProps = (context) => {
 }
 
 export default Manage
-
-const types = ['Stake', 'Unstake'] as const
-function StakeAndUnstake({
-    pool: { managerAddress, liquidityTokenAddress, liquidityTokenDecimals },
-    poolAddress,
-    account,
-}: {
-    pool: Pool
-    poolAddress: string
-    account: string
-}) {
-    const [type, setType] = useState<typeof types[number]>('Stake')
-
-    const [stats] = useStatsState(poolAddress)
-    const [info, refetchManagerInfo] = useManagerInfo(poolAddress)
-
-    const max = useMemo(() => {
-        if (type === 'Stake') return undefined
-
-        if (info) return BigNumber.from(info.unstakable)
-
-        return undefined
-    }, [type, info])
-
-    const isNotManager = managerAddress !== account
-
-    const { form, value } = useAmountForm({
-        type,
-        onSumbit:
-            type === 'Stake'
-                ? (contract, amount) => contract.stake(amount)
-                : (contract, amount) => contract.unstake(amount),
-        refetch: () =>
-            Promise.all([
-                refetchManagerInfo(),
-                refetchStatsIfUsed(poolAddress),
-            ]),
-        poolAddress,
-        liquidityTokenAddress,
-        liquidityTokenDecimals,
-        disabled: Boolean(isNotManager),
-        max,
-    })
-
-    return (
-        <Box
-            loading={Boolean(type === 'Unstake' && account ? !info : false)}
-            overlay={isNotManager ? 'Only manager can stake' : undefined}
-        >
-            <Tabs tabs={types} currentTab={type} setCurrentTab={setType}></Tabs>
-
-            {form}
-
-            {type === 'Stake' ? (
-                <Alert
-                    style="warning"
-                    title="You should not stake unless you are prepared to sustain a total loss of the money you have invested plus any commission or other transaction charges"
-                />
-            ) : (
-                <ExitAlert
-                    value={value}
-                    verb="unstaking"
-                    feePercent={stats ? stats.exitFeePercent : 0}
-                />
-            )}
-        </Box>
-    )
-}
 
 interface BaseLoanRequest {
     id: number
